cbfca8f4ebff33064bc05bee19878866fa89e307
lopera.william@gmail.com <William>
2022-02-03 09:30:02 -0500

    agregar deploy marketing en prod

--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   output: {
     clean: true,
@@ -18,4 +20,10 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -20,10 +19,6 @@ const devConfig = {
       filename: "remoteEntity.js",
       exposes: { "./MarketingApp": "./src/bootstrap" },
       shared: packageJson.dependencies,
-    }),
-
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
